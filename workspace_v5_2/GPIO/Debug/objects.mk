bdc9f148f74653c10d0516d6ea456ae62cdf0dd2
tbh111 <1308751722@qq.com>
2023-04-12 14:45:38 +0800

    Delete workspace_v5_2/GPIO/Debug directory

--- workspace_v5_2/GPIO/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := $(GEN_CMDS__FLAG) -l"libc.a" -l"C:/ti/pdk_C6678_1_1_2_5/packages/ti/platform/evmc6678l/platform_lib/lib/debug/ti.platform.evm6678l.ae66"
-
